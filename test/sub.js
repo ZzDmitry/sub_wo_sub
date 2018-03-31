6d4c85fc2888b7db83f270b893e558ee5910bb47
ZzDmitry <ZzDmitry@yandex.ru>
2018-03-31 17:57:43 +0600

    Simple implementation, tests

---
+++ test/sub.js
@@ -0,0 +1,73 @@
+const assert = require('assert');
+
+const {sub: subWithMinus} = require('../sub-with-minus');
+
+function makeDescribe(name, subFunction) {
+	
+	describe(name, () => {
+
+		it('must sub lesser from bigger', () => {
+			assert.equal(subFunction(5, 2), 3);
+		});
+
+		it('must sub bigger from lesser', () => {
+			assert.equal(subFunction(2, 5), -3);
+		});
+
+		it('must sub equal', () => {
+			assert.equal(subFunction(5, 5), 0);
+		});
+
+		it('must sub negatives lesser from bigger', () => {
+			assert.equal(subFunction(-5, -2), -3);
+		});
+
+		it('must sub negatives bigger from lesser', () => {
+			assert.equal(subFunction(-2, -5), 3);
+		});
+
+		it('must sub negatives equal', () => {
+			assert.equal(subFunction(-5, -5), 0);
+		});
+
+		it('must sub mixed sign lesser from bigger', () => {
+			assert.equal(subFunction(5, -2), 7);
+			assert.equal(subFunction(2, -5), 7);
+		});
+
+		it('must sub mixed sign bigger from lesser', () => {
+			assert.equal(subFunction(-2, 5), -7);
+			assert.equal(subFunction(-5, 2), -7);
+		});
+
+		it('must sub float lesser from bigger', () => {
+			assert.equal(subFunction(5.25, 2.75), 2.5);
+		});
+
+		it('must sub float bigger from lesser', () => {
+			assert.equal(subFunction(2.25, 5.75), -3.5);
+		});
+
+		it('must sub float equal', () => {
+			assert.equal(subFunction(0.5, 0.5), 0);
+		});
+
+		function roundDigits(a, n) {
+			const pow10 = Math.pow(10, n);
+			return Math.round(a * pow10) / pow10;
+		}
+
+		it('must sub infinite fraction float lesser from bigger', () => {
+			assert.notEqual(subFunction(0.3, 0.2), 0.1);
+			assert.equal(roundDigits(subFunction(0.3, 0.2), 5), 0.1);
+		});
+
+		it('must sub infinite fraction bigger from lesser', () => {
+			assert.notEqual(subFunction(0.1, 0.3), -0.2);
+			assert.equal(roundDigits(subFunction(0.1, 0.3), 5), -0.2);
+		});
+
+	});
+}
+
+makeDescribe('test with minus', subWithMinus);
